c5ffcede7ee9faf8c8e6a40307795fb9c73b53fa
Agustin Abreu <agustinabreu@gmail.com>
2016-12-30 12:14:03 -0300

    fix

--- spaceoutvr/migrations/0022_populate_uuid_values.py
+++ spaceoutvr/migrations/0022_populate_uuid_values.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-k = 0
 def gen_username(apps, schema_editor):
+    k = 0
     MyModel = apps.get_model('spaceoutvr', 'SpaceoutUser')
     for row in MyModel.objects.all():
         row.user_name = "spaceout_user_%" % k
