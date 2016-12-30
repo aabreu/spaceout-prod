1885e575c202af99ce98948793ac7ca4c9b23581
Agustin Abreu <agustinabreu@gmail.com>
2016-12-30 13:36:58 -0300

    test

--- spaceoutvr_django/settings.py
+++ spaceoutvr_django/settings.py
@@ -19,11 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECUREKEY')
-# SECRET_KEY = "efhiuwehfiuwehfiuehuighiowejfioewjio"
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECUREKEY')
+SECRET_KEY = "efhiuwehfiuwehfiuehuighiowejfioewjio"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 IS_LOCAL = False
 TEMPLATE_DEBUG = False
 
